583041826c657107cee99b4f612b28631f5faac5
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2024-06-16 03:08:10 +0300

    transfer and withdraw

--- app/src/main/java/by/andrew/pokemonsworld/DetailsActivity.kt
+++ app/src/main/java/by/andrew/pokemonsworld/DetailsActivity.kt
@@ -1,9 +1,11 @@
 package by.andrew.pokemonsworld
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import by.andrew.pokemonsworld.databinding.ActivityDetailsBinding
+import by.andrew.pokemonsworld.repository.PokemonsRepository
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -11,13 +13,25 @@ class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonBack.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        binding.buttonBackView.setOnClickListener {
+            finish()
+        }
+
+        val pokemonId = intent.extras?.getInt("id")
+        val pokemon = PokemonsRepository.getPokemonById(pokemonId)
+        if (pokemon != null) {
+            binding.imageView.setImageResource(pokemon.image)
+            binding.nameTextView.text = pokemon.name
+            binding.weightTextView.text = "${pokemon.weight} kg"
+            binding.heightTextView.text = "${pokemon.height} cm"
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, pokemon.types)
+            binding.typesListView.adapter = adapter
+        } else {
+            Toast.makeText(this, "Invalid pokemon id", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
--- app/src/main/java/by/andrew/pokemonsworld/MainActivity.kt
+++ app/src/main/java/by/andrew/pokemonsworld/MainActivity.kt
@@ -10,12 +10,49 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.pokemonName.setOnClickListener {
-            val intent = Intent(this, DetailsActivity::class.java)
+        binding.bulbasaur.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java).apply {
+                putExtra("id", 1)
+            }
+            startActivity(intent)
+        }
+        binding.ivynsaur.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java).apply {
+                putExtra("id", 2)
+            }
+            startActivity(intent)
+        }
+        binding.venusaur.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java).apply {
+                putExtra("id", 3)
+            }
+            startActivity(intent)
+        }
+        binding.charmander.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java).apply {
+                putExtra("id", 4)
+            }
+            startActivity(intent)
+        }
+        binding.charmeleon.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java).apply {
+                putExtra("id", 5)
+            }
+            startActivity(intent)
+        }
+        binding.charizard.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java).apply {
+                putExtra("id", 6)
+            }
+            startActivity(intent)
+        }
+        binding.squirtle.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java).apply {
+                putExtra("id", 7)
+            }
             startActivity(intent)
         }
     }

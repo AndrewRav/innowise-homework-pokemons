5d1947a8441e3a89bb7405799229328b7dae1410
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2024-06-20 18:30:30 +0300

    change check to null

--- app/src/main/java/by/andrew/pokemonsworld/DetailsActivity.kt
+++ app/src/main/java/by/andrew/pokemonsworld/DetailsActivity.kt
@@ -1,7 +1,6 @@
 package by.andrew.pokemonsworld
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,7 +22,7 @@ class DetailsActivity : AppCompatActivity() {
 
         val pokemonId = intent.extras?.getInt("id")
         val pokemon = PokemonsRepository.getPokemonById(pokemonId)
-        if (pokemon != null) {
+        pokemon?.let {
             binding.imageView.setImageResource(pokemon.imageRes)
             binding.nameTextView.text = pokemon.name
             binding.weightTextView.text = "${pokemon.weight} kg"
@@ -31,7 +30,7 @@ class DetailsActivity : AppCompatActivity() {
             val adapter = TypeAdapter(pokemon.types)
             binding.typesRecyclerView.layoutManager = LinearLayoutManager(this)
             binding.typesRecyclerView.adapter = adapter
-        } else {
+        } ?: run {
             Toast.makeText(this, "Invalid pokemon id", Toast.LENGTH_SHORT).show()
             finish()
         }

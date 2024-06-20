a313cf266b7c5fef21f209c21ffac0b76a7754f3
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2024-06-20 11:26:16 +0300

    add recycler view with adapter

--- app/src/main/java/by/andrew/pokemonsworld/DetailsActivity.kt
+++ app/src/main/java/by/andrew/pokemonsworld/DetailsActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.andrew.pokemonsworld.databinding.ActivityDetailsBinding
 import by.andrew.pokemonsworld.repository.PokemonsRepository
 
@@ -23,12 +24,13 @@ class DetailsActivity : AppCompatActivity() {
         val pokemonId = intent.extras?.getInt("id")
         val pokemon = PokemonsRepository.getPokemonById(pokemonId)
         if (pokemon != null) {
-            binding.imageView.setImageResource(pokemon.image)
+            binding.imageView.setImageResource(pokemon.imageRes)
             binding.nameTextView.text = pokemon.name
             binding.weightTextView.text = "${pokemon.weight} kg"
             binding.heightTextView.text = "${pokemon.height} cm"
-            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, pokemon.types)
-            binding.typesListView.adapter = adapter
+            val adapter = TypeAdapter(pokemon.types)
+            binding.typesRecyclerView.layoutManager = LinearLayoutManager(this)
+            binding.typesRecyclerView.adapter = adapter
         } else {
             Toast.makeText(this, "Invalid pokemon id", Toast.LENGTH_SHORT).show()
             finish()
